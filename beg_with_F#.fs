6d5a79f1f6325ddcc041d0f7c35a8d9bb59e0d24
Kseniya-Gonta <redlioness.ru@mail.ru>
2014-03-28 11:16:02 +0300

    Update beg_with_F#.fs

--- beg_with_F#.fs
+++ beg_with_F#.fs
@@ -58,4 +58,13 @@ let mathPuzzle =
 
 printf "x = %A" (mathPuzzle)
 
+//передать функцию, как аргумент
+let add x y = x + y
+
+let result1 = add 4 5
+let result2 = add 6 7 
+
+let finalResult = add result1 result2
+printfn "answer = %A" (finalResult)
+
 
