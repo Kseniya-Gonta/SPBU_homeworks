d8645fe09056ee435b195e0c0101a034f7605f4d
Kseniya-Gonta <redlioness.ru@mail.ru>
2016-02-27 17:01:45 +0300

    It replace sdf with qml. TODO: remowe attributes. Add writing to file

--- main.cpp
+++ main.cpp
@@ -17,12 +17,40 @@ void parsePictures(QDomNode doc)
 		{
 			qDebug() << "found picture";
 			qDebug() << elem.tagName();
+			//QDomText text = elem.toText();
 			Translator t;
-			QFile files("test.qml");
-			files.open(QIODevice::WriteOnly | QIODevice::Text);
-			files.write(QString("import QtQuick 2.0 \n").toLatin1());
-			files.write(QString("import CustomComponents 1.0 \n").toLatin1());
-			files.write(t.startTranslate(elem).toLatin1());
+			QString result = "";
+			result += (QString("import QtQuick 2.0 \n").toLatin1());
+			result += (QString("import CustomComponents 1.0 \n").toLatin1());
+			result += (t.startTranslate(elem).toLatin1());
+			qDebug() << result;
+			//elem.childNodes().at(0).toText().setData(result);
+			//elem.appendChild(QDomNode(result));
+			//text.setData(result);
+			//elem.setNodeValue(result);
+			//elem.firstChild().setNodeValue(result);
+			//elem.childNodes().at(1).setNodeValue(result);
+			//doc.removeChild( doc.firstChildElement( ));
+			//QDomElement newNodeTag = elem.createElement(result);
+			//doc.parentNode().replaceChild(doc, newNodeTag);
+
+			while (!elem.firstChild().isNull()) {
+				elem.removeChild(elem.firstChild());
+			}
+			elem.setNodeValue(result);
+			QDomText newNodeTag = elem.ownerDocument().createTextNode(result);
+			elem.appendChild(newNodeTag);
+			//QDomNode parent = doc.parentNode();
+			//parent.firstChildElement("picture").firstChild().setNodeValue(result);
+			//elem.firstChild()
+			//doc.parentNode().removeChild( doc );
+			//QDomNode zzxc;
+			//zzxc.setNodeValue("asdf");
+			//parent.appendChild( zzxc);
+			//doc.setNodeValue("asdf");
+			//doc.parentNode().setNodeValue("asdf");
+			//qDebug() << doc.parentNode().toS
+			//doc.replaceChild()
 		}
 		doc = doc.nextSibling();
 	}
@@ -32,11 +60,11 @@ void parseGraphics(QDomNode doc)
 {
 	while (!doc.isNull())
 	{
-		qDebug() << "nextcicle";
+		//qDebug() << "nextcicle";
 		QDomElement elem = doc.toElement();
 		if(elem.tagName() == "graphics")
 		{
-			qDebug() << "found graphics";
+			//qDebug() << "found graphics";
 			parsePictures(elem.firstChild());
 		}
 		doc = doc.nextSibling();
@@ -47,11 +75,11 @@ void parseNode(QDomNode doc)
 {
 	while (!doc.isNull())
 	{
-		qDebug() << "nextcicle";
+		//qDebug() << "nextcicle";
 		QDomElement elem = doc.toElement();
 		if(elem.tagName() == "node")
 		{
-			qDebug() << "found node";
+			//qDebug() << "found node";
 			parseGraphics(elem.firstChild());
 		}
 		doc = doc.nextSibling();
@@ -62,11 +90,11 @@ void parseDiagram(QDomNode doc)
 {
 	while (!doc.isNull())
 	{
-		qDebug() << "nextcicle";
+		//qDebug() << "nextcicle";
 		QDomElement elem = doc.toElement();
 		if(elem.tagName() == "graphicTypes")
 		{
-			qDebug() << "found graphicTypes";
+			//qDebug() << "found graphicTypes";
 			parseNode(elem.firstChild());
 		}
 		doc = doc.nextSibling();
@@ -77,11 +105,11 @@ void parseMetamodel(QDomNode doc)
 {
 	while (!doc.isNull())
 	{
-		qDebug() << "nextcicle";
+		//qDebug() << "nextcicle";
 		QDomElement elem = doc.toElement();
 		if(elem.tagName() == "diagram")
 		{
-			qDebug() << "found diagram";
+			//qDebug() << "found diagram";
 			parseDiagram(elem.firstChild());
 		}
 		doc = doc.nextSibling();
@@ -90,29 +118,33 @@ void parseMetamodel(QDomNode doc)
 
 int main(int argc, char *argv[])
 {
-	QFile file("robotsMetamodel.xml");
-	if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+	QFile file("check.xml");
+	if (!file.open(QIODevice::ReadWrite | QIODevice::Text))
 		return 35;
 	QDomDocument newdoc;
 	if (!newdoc.setContent(&file)) {
 		file.close();
 		return 15;
 	}
+	file.close();
 
 	QDomElement elem;
-	QString result = "+++++";
 	QDomNode doc = newdoc.firstChild();
 	while (!doc.isNull())
 	{
-		qDebug() << "cicle";
+		//qDebug() << "cicle";
 		elem = doc.toElement();
 		if(elem.tagName() == "metamodel")
 		{
-			qDebug() << "found metamodel";
+			//qDebug() << "found metamodel";
 			parseMetamodel(elem.firstChild());
 		}
 		doc = doc.nextSibling();
 
 	}
+
+
+	qDebug() << newdoc.toString();
+
 	return 0;
 }
